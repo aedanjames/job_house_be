10b1719c6e59f7989874fad25c055b3fe9d300c5
Aedan Yturralde <aedanyturralde@gmail.com>
2022-04-14 17:43:17 -0600

    job service troubleshooting

--- app/services/job_service.rb
+++ app/services/job_service.rb
@@ -1,10 +1,22 @@
 class JobService
-  def self.conn
-    Faraday.new("https://developer.adzuna.com")
-  end
 
-  def self.get_job_info(state, city)
-    response = conn.get("/v1/api/jobs/us/search/1?app_id=#{ENV['app_id']}&app_key=#{ENV['app_key']}&salary_min=100000&salary_max=200000&location0=us&location1=#{state}&location2=#{city}")
-    JSON.parse(response.body, symbolize_names: true)
+  def self.get_url(url, city = nil, state = nil)
+    conn = Faraday.new(url: 'https://api.adzuna.com/v1/api') do |faraday|
+      faraday.params[:app_id] = "5e859b54"
+      faraday.params[:app_key] = "9b7837242edf32ddec42da936025659e"
+      faraday.params[:salary_min] = 100000
+      faraday.params[:salary_max] = 200000
+      faraday.params[:location0] = "us"
+      faraday.params[:location1] = state
+      faraday.params[:location2] = city
+    end
+
+    response = conn.get(url)
+    help_me = JSON.parse(response.body, symbolize_names: true)
+  end 
+
+  def self.get_job_info(city, state)
+    get_url('/v1/api/jobs/us/search/1', state, city)
   end
 end
+
--- spec/services/job_service_spec.rb
+++ spec/services/job_service_spec.rb
@@ -2,21 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'job service' do
   it 'can connect to a job service api' do
-    results = File.read('spec/fixtures/jobs_sf.json')
-    state = "california"
-    city = "san francisco"
-
-    stub_request(:get, "https://developer.adzuna.com/v1/api/jobs/us/search/1?app_id=#{ENV['app_id']}&app_key=#{ENV['app_key']}&location0=us&location1=california&location2=san%20francisco&salary_max=200000&salary_min=100000").
-         with(
-           headers: {
-       	  'Accept'=>'*/*',
-       	  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-       	  'User-Agent'=>'Faraday v2.2.0'
-           }).
-         to_return(status: 200, body: results, headers: {})
-
-    search = JobService.get_job_info(state, city)
-
-    expect(search).to be_a(Hash)
+    VCR.use_cassette(".get_job_info") do
+      jobs = JobService.get_job_info("california", "san francisco")
+    end
   end
 end
