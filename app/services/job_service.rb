53b25ee300958a53601c1aa0f277a7d99ddc21c5
Sam Smith <samlsmith424@gmail.com>
2022-04-14 16:34:04 -0700

    Feat: city and state params in jobs controller

--- app/services/job_service.rb
+++ app/services/job_service.rb
@@ -1,14 +1,15 @@
 class JobService
   def self.get_url(url, city = nil, state = nil)
-    conn = Faraday.new(url: 'https://api.adzuna.com/v1/api') do |faraday|
+    conn = Faraday.new(url: 'https://api.adzuna.com') do |faraday|
     # conn = Faraday.new(url: 'https://api.adzuna.com/v1/api/jobs/us/search/1') do |faraday|
       faraday.params[:app_id] = ENV['app_id']
       faraday.params[:app_key] = ENV['app_key']
-      faraday.params[:salary_min] = 100000
-      faraday.params[:salary_max] = 200000
+      faraday.params[:salary_min] = 0
+      faraday.params[:salary_max] = 10000000
       faraday.params[:location0] = "us"
       faraday.params[:location1] = state
       faraday.params[:location2] = city
+      faraday.params[:results_per_page] = 50
     end
     response = conn.get(url)
     help_me = JSON.parse(response.body, symbolize_names: true)
