b9d3831422ad55501794fe2cfd7a22e0555d45f0
Austin Moore <austinchristianmoore@gmail.com>
2022-04-14 12:21:58 -0400

    Fix: Remove pry

--- app/facades/job_facade.rb
+++ app/facades/job_facade.rb
@@ -1,7 +1,7 @@
 class JobFacade
   def self.find_jobs(state, city)
     jobs = JobService.get_job_info(state, city)
-    binding.pry
+    
     jobs[:results].map do |job_info|
       Job.new(job_info)
     end
