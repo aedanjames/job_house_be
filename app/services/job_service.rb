81d85a741bfa1774ec3a7249b3144454f1756ab2
Sam Smith <samlsmith424@gmail.com>
2022-04-14 15:54:12 -0700

    Feature: vcr on job service

--- app/services/job_service.rb
+++ app/services/job_service.rb
@@ -1,10 +1,21 @@
 class JobService
-  def self.conn
-    Faraday.new("https://developer.adzuna.com")
+  def self.get_url(url, city = nil, state = nil)
+    conn = Faraday.new(url: 'https://api.adzuna.com/v1/api') do |faraday|
+    # conn = Faraday.new(url: 'https://api.adzuna.com/v1/api/jobs/us/search/1') do |faraday|
+      faraday.params[:app_id] = ENV['app_id']
+      faraday.params[:app_key] = ENV['app_key']
+      faraday.params[:salary_min] = 100000
+      faraday.params[:salary_max] = 200000
+      faraday.params[:location0] = "us"
+      faraday.params[:location1] = state
+      faraday.params[:location2] = city
+    end
+    response = conn.get(url)
+    help_me = JSON.parse(response.body, symbolize_names: true)
   end
 
-  def self.get_job_info(state, city)
-    response = conn.get("/v1/api/jobs/us/search/1?app_id=#{ENV['app_id']}&app_key=#{ENV['app_key']}&salary_min=100000&salary_max=200000&location0=us&location1=#{state}&location2=#{city}")
-    JSON.parse(response.body, symbolize_names: true)
+  def self.get_job_info(city, state)
+    # get_url('/v1/api/jobs/us/search/1', state, city)
+    get_url('/v1/api/jobs/us/search/1', state, city)
   end
 end
