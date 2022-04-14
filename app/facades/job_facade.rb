261e72d8aca9a9cc96c1cdd3be1658403551795a
Austin Moore <austinchristianmoore@gmail.com>
2022-04-14 12:20:28 -0400

    Fix: Fix facade access key

--- app/facades/job_facade.rb
+++ app/facades/job_facade.rb
@@ -1,8 +1,8 @@
 class JobFacade
   def self.find_jobs(state, city)
     jobs = JobService.get_job_info(state, city)
-
-    jobs[:data].map do |job_info|
+    binding.pry
+    jobs[:results].map do |job_info|
       Job.new(job_info)
     end
   end
