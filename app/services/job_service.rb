7f04952069d6595dbe6053b43d875055ae9a9f5d
Sam Smith <samlsmith424@gmail.com>
2022-04-14 19:20:27 -0700

    Test: vcr sf houses

--- app/services/job_service.rb
+++ app/services/job_service.rb
@@ -1,7 +1,6 @@
 class JobService
   def self.get_url(url, city = nil, state = nil)
     conn = Faraday.new(url: 'https://api.adzuna.com') do |faraday|
-    # conn = Faraday.new(url: 'https://api.adzuna.com/v1/api/jobs/us/search/1') do |faraday|
       faraday.params[:app_id] = ENV['app_id']
       faraday.params[:app_key] = ENV['app_key']
       faraday.params[:salary_min] = 0
@@ -16,7 +15,6 @@ class JobService
   end
 
   def self.get_job_info(city, state)
-    # get_url('/v1/api/jobs/us/search/1', state, city)
     get_url('/v1/api/jobs/us/search/1', state, city)
   end
 end
