3f580d3ade218ad988e3db5b0a67722e2be41bee
Sam Smith <samlsmith424@gmail.com>
2022-04-15 08:54:41 -0700

    Feat: dynamic where param

--- app/controllers/api/v1/jobs_controller.rb
+++ app/controllers/api/v1/jobs_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::JobsController < ApplicationController
   def index
-    jobs = JobFacade.find_jobs(params[:state], params[:city])
+    jobs = JobFacade.find_jobs(params[:where])
     render json: JobSerializer.new(jobs)
   end
 end
