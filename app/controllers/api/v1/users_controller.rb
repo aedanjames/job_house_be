4122b35952561b3b06e560fb61b9791583c46b53
Richard LaBrecque <RichardlaBreque@gmail.com>
2022-04-15 11:58:44 -0400

    upadate find or create by

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UsersController < ApplicationController
   def show
-    user = User.find_or_create_by(params[:email])
+    user = User.find_or_create_by(email: params[:email])
     render json: UserSerializer.new(user)
   end
 end
