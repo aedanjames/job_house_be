bc647b4acfe5c36f606c96a331ace9057b2d98b8
Sam Smith <samlsmith424@gmail.com>
2022-04-20 15:22:42 -0700

    Test: house attributes

--- app/controllers/api/v1/job_houses_controller.rb
+++ app/controllers/api/v1/job_houses_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::JobHousesController < ApplicationController
   def index
     @job = Job.find(params[:id])
     @houses = HouseFacade.find_houses(@job.location_hash, @job.mortgage_calculator)
-    # require "pry"; binding.pry
+
     render json: JobHouseSerializer.new(@houses)
   end
 end
