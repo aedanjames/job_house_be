88dbe539574f036976b3e88635cc42abddb6f7e9
Richard LaBrecque <RichardlaBreque@gmail.com>
2022-04-21 11:53:17 -0400

    test: create user_job_house

--- spec/requests/api/v1/user_job_house_request_spec.rb
+++ spec/requests/api/v1/user_job_house_request_spec.rb
@@ -5,5 +5,8 @@ RSpec.describe 'user_job_house' do
     user = User.create!(email: "someemail")
     job = user.jobs.create!(salary: 1000000, location: "Houston, TX", company: "Texas Tech", contact: "Brad Chad", api_job_id: 1111, title: "Tech Bro")
     post "/api/v1/jobs/houses?email=someemail&job_id=#{job.id}&mls_id=1005192"
+    user_job = UserJob.find_by(user_id: user.id, job_id: job.id)
+    ujh = UserJobHouse.find_by(user_job_id: user_job.id)
+    expect(ujh.house_id).to eq(1005192)
   end
 end
