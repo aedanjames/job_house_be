7951b05973729a191781f86f4cce80fd3d9575cd
Sam Smith <samlsmith424@gmail.com>
2022-04-21 09:14:20 -0700

    Feat: user jobs controller destory action

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/users', to: 'users#show'
       #resources :users, only: [:show]
+      delete '/user/:user_id/jobs/:job_id', to: 'user_jobs#destroy'
       resources :jobs, only: [:index, :show, :create]
 
       get '/jobs/:id/houses', to: 'job_houses#index'
