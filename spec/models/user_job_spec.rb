454337aa1e7415f5c9ee772f3664a1cdf8074363
Sam Smith <samlsmith424@gmail.com>
2022-05-04 15:19:22 -0700

    Test: UserJob relationships

--- spec/models/user_job_spec.rb
+++ spec/models/user_job_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe UserJob do
+RSpec.describe UserJob, type: :model do
+  describe 'relationships' do
+    it { should belong_to :user }
+    it { should belong_to :job }
+    it { should have_many :user_job_houses }
+  end
+
   it 'deletes user_job_houses on destroy' do
 
   end
