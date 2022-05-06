6b77d1a3ecf8ea0231ff1243db599bffef44511f
Aedan Yturralde <aedanyturralde@gmail.com>
2022-05-06 12:29:36 -0600

    feature: user delete UserHouse endpoint. Works in localhost in combination with FE

--- app/controllers/api/v1/user_houses_controller.rb
+++ app/controllers/api/v1/user_houses_controller.rb
@@ -15,4 +15,15 @@ class Api::V1::UserHousesController < ApplicationController
     saved_homes = HouseFacade.find_saved_homes(houses)
     render json: JobHouseSerializer.new(saved_homes)
   end
+
+  def destroy
+    user = User.find_by(email: params[:email])
+    user_house = UserHouse.find_by(user_id: user.id, house_id: params[:mls_id])
+    if user.nil? || user_house.nil?
+      render response: :bad_request
+    else
+      user_house.destroy
+      render response: 200
+    end
+  end
 end
